4e2f2c9964ab87a074c018b294064ab42ceb6ea1
mohamed ragap <133944965+sqlrnw@users.noreply.github.com>
2025-08-30 19:19:29 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "app" {
-  ami                    = "ami-0c02fb55956c7d316"  # Free Tier-compatible AMI in us-east-1
+  ami                    = "ami-0c02fb55956c7d316"  
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   user_data = <<-EOF
-              #!/bin/bash
+            
               yum install -y httpd
               echo "<h1>Deployed from Terraform</h1>" > /var/www/html/index.html
               systemctl start httpd
